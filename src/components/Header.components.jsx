dd08ea37d1ad406c7a0f11a273e61c14754efb1c
Santiago Monaco <santiago.monaco33@gmail.com>
2021-12-07 10:10:20 -0300

    add globalStyle

---
+++ src/components/Header.components.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderC = styled.div`
+  color: var(--primary);
+`;
+
+const Header = () => {
+  return (
+      <HeaderC>Esto es el header</HeaderC>
+  );
+};
+
+export default Header;
